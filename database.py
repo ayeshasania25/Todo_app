acc53047aeedcd263882fab837577db07c4da6f0
ayeshasania25 <ayeshasania25@gmail.com>
2026-02-01 11:33:30 +0530

    Delete database.py

--- database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-db_url="postgresql://postgres:Haniya@1" \
-"@localhost:5432/TestDB"
-engine=create_engine(db_url)
-Session=sessionmaker( autocommit=False,autoflush=False,bind=engine)
